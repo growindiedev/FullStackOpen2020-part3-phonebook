f940f28065f795c75e12183f78c9217fb656eb0a
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-10-23 18:27:02 +0530

    front-end + back-end deployment

---
+++ build/precache-manifest.a71c275f07027a643fc2a8a7cd39bd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c843af0f0604169475ff0ada424af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "973bfcba22503aadd802",
+    "url": "/static/css/main.158169f5.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973bfcba22503aadd802",
+    "url": "/static/js/main.1e394f23.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(":method :url :status :response-time ms :body"))
 
 let persons = [
